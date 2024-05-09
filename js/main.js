ea3218cbce6d282d0c4cd78e4f3589b2f144c2df
Pedro Soares <pedrossgarcia88@gmail.com>
2024-05-09 16:19:41 -0300

    primeiro commit, form para postar cursos criada

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const form = document.querySelector("#cursoForm");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let titulo = document.getElementById("titulo").value;
+  let duracao = document.getElementById("duracao").value;
+  let nivel = document.getElementById("nivel").value;
+  let professor = document.getElementById("professor").value;
+  let experiencia = document.getElementById("experiencia").value;
+  let certificado = document.getElementById("certificado").value;
+  let descricao = document.getElementById("descricao").value;
+
+  function gerarIdUnico() {
+    return "_" + Math.random().toString(36).substr(2, 9);
+  }
+
+  let novoCurso = {
+    id: gerarIdUnico(),
+    titulo: titulo,
+    duracao: duracao,
+    nivel: nivel,
+    professsor: {
+      nome: professor,
+      experiencia: experiencia,
+    },
+    certificado: certificado,
+    descricao: descricao,
+  };
+
+  let cursos = JSON.parse(localStorage.getItem("cursos")) || [];
+  cursos.push(novoCurso);
+  localStorage.setItem("cursos", JSON.stringify(cursos));
+
+  window.location.href = "cursos.html";
+});
