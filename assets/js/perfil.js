ea9afa94324754c8a500023a504d79a0309a4631
CaioVKodato <caiovictorkodatot@gmail.com>
2024-06-06 20:48:34 -0300

    Version mais atualizada do perfil

--- assets/js/perfil.js
+++ assets/js/perfil.js
@@ -1,24 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var usuarios = JSON.parse(localStorage.getItem('usuarios'));
-    var usuarioLogado = JSON.parse(localStorage.getItem('userLogado'));
+document.addEventListener('DOMContentLoaded', () => {
+
+  const usuarioLogado = JSON.parse(localStorage.getItem('userLogado'));
   
-    if (usuarios && usuarioLogado) {
-      var usuario = usuarios.find(function (user) {
-        return user.userCad === usuarioLogado.user;
-      });
-  
-      if (usuario) {
-        document.getElementById('nomeCompleto').textContent = usuario.nomeCad;
-        document.getElementById('userName').textContent = usuario.userCad;
-        document.getElementById('genero').textContent = usuario.generoCad;
-        document.getElementById('dataNascimento').textContent = usuario.dataNascimentoCad;
-        document.getElementById('nivelConhecimento').textContent = usuario.nivelCad;
-      }
-    }
-  });
-  
-  function editarPerfil() {
-    // Redirecionar o usuário para a tela de edição de perfil ou implementar a funcionalidade de edição aqui
-    // Por exemplo: window.location.href = 'editarPerfil.html';
+  if (usuarioLogado) {
+    document.getElementById('userName').textContent = usuarioLogado.userCad;
+    document.getElementById('nome').textContent = usuarioLogado.nomeCad;
+    document.getElementById('usuario').textContent = usuarioLogado.userCad;
+    document.getElementById('dataNascimento').textContent = usuarioLogado.dataNascimentoCad;
+    document.getElementById('nivel').textContent = usuarioLogado.nivelCad;
+    document.getElementById('genero').textContent = usuarioLogado.generoCad;
+  } else {
+    // Se não houver usuário logado, redirecionar para a página de login
+    window.location.href = 'signin.html';
   }
+
+
+});
+
+function editarUsuario() {
   
+  const usuarioLogado = JSON.parse(localStorage.getItem('usuarios'));
+  
+  const novoUsuario = prompt('Digite seu novo nome de usuário:');
+  
+  if (novoUsuario) {
+      
+      usuarioLogado.userCad = novoUsuario;
+      localStorage.setItem('usuarios', JSON.stringify(usuarioLogado));
+      
+      document.getElementById('usuario').textContent = novoUsuario;
+      document.getElementById('userName').textContent = novoUsuario;
+
+      alert('Nome de usuário atualizado com sucesso!');
+  } else {
+      alert('Nome de usuário não alterado.');
+  }
+}
+
+function editarSenha() {
+  // Recupere a senha atual do usuário logado
+  const usuarioLogado = JSON.parse(localStorage.getItem('userLogado'));
+  const senhaAtual = usuarioLogado.senha;
+
+  // Solicite ao usuário que insira a nova senha
+  const novaSenha = prompt('Digite sua nova senha:');
+
+  // Verifique se a nova senha não está vazia e se é diferente da senha atual
+  if (novaSenha && novaSenha !== senhaAtual) {
+    // Atualize a senha no localStorage
+    usuarioLogado.senha = novaSenha;
+    localStorage.setItem('userLogado', JSON.stringify(usuarioLogado));
+    alert('Senha atualizada com sucesso!');
+  } else if (novaSenha === senhaAtual) {
+    alert('A nova senha não pode ser igual à senha atual.');
+  } else {
+    alert('Senha não alterada.');
+  }
+}
--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,147 +1,143 @@
-let btn = document.querySelector('#verSenha')
-let btnConfirm = document.querySelector('#verConfirmSenha')
+let btn = document.querySelector('#verSenha');
+let btnConfirm = document.querySelector('#verConfirmSenha');
 
+let nome = document.querySelector('#nome');
+let labelNome = document.querySelector('#labelNome');
+let validNome = false;
 
-let nome = document.querySelector('#nome')
-let labelNome = document.querySelector('#labelNome')
-let validNome = false
+let dataNascimento = document.querySelector('#dataNascimento');
+let labelDataNascimento = document.querySelector('#labelDataNascimento');
+let validDataNascimento = false;
 
-let dataNascimento = document.querySelector('#dataNascimento')
-let labelDataNascimento = document.querySelector('#labelDataNascimento')
-let validDataNascimento = false
+let usuario = document.querySelector('#usuario');
+let labelUsuario = document.querySelector('#labelUsuario');
+let validUsuario = false;
 
-let usuario = document.querySelector('#usuario')
-let labelUsuario = document.querySelector('#labelUsuario')
-let validUsuario = false
+let senha = document.querySelector('#senha');
+let labelSenha = document.querySelector('#labelSenha');
+let validSenha = false;
 
-let senha = document.querySelector('#senha')
-let labelSenha = document.querySelector('#labelSenha')
-let validSenha = false
+let confirmSenha = document.querySelector('#confirmSenha');
+let labelConfirmSenha = document.querySelector('#labelConfirmSenha');
+let validConfirmSenha = false;
 
-let confirmSenha = document.querySelector('#confirmSenha')
-let labelConfirmSenha = document.querySelector('#labelConfirmSenha')
-let validConfirmSenha = false
-
-let msgErro = document.querySelector('#msgError')
-let msgSuccesso = document.querySelector('#msgSuccess')
+let msgErro = document.querySelector('#msgError');
+let msgSuccesso = document.querySelector('#msgSuccess');
 
 nome.addEventListener('keyup', () => {
-  if(nome.value.length <= 2){
-    labelNome.setAttribute('style', 'color: red')
-    labelNome.innerHTML = 'Nome *Insira no minimo 3 caracteres'
-    nome.setAttribute('style', 'border-color: red')
-    validNome = false
+  if (nome.value.length <= 2) {
+    labelNome.setAttribute('style', 'color: red');
+    labelNome.innerHTML = 'Nome *Insira no mínimo 3 caracteres';
+    nome.setAttribute('style', 'border-color: red');
+    validNome = false;
   } else {
-    labelNome.setAttribute('style', 'color: green')
-    labelNome.innerHTML = 'Nome'
-    nome.setAttribute('style', 'border-color: green')
-    validNome = true
+    labelNome.setAttribute('style', 'color: green');
+    labelNome.innerHTML = 'Nome';
+    nome.setAttribute('style', 'border-color: green');
+    validNome = true;
   }
-})
+});
 
 dataNascimento.addEventListener('change', () => {
-  if(dataNascimento.value === ""){
-    labelDataNascimento.setAttribute('style', 'color: red')
-    labelDataNascimento.innerHTML = 'Data de Nascimento *Campo obrigatório'
-    dataNascimento.setAttribute('style', 'border-color: red')
-    validDataNascimento = false
+  if (dataNascimento.value === "") {
+    labelDataNascimento.setAttribute('style', 'color: red');
+    labelDataNascimento.innerHTML = 'Data de Nascimento *Campo obrigatório';
+    dataNascimento.setAttribute('style', 'border-color: red');
+    validDataNascimento = false;
   } else {
-    labelDataNascimento.setAttribute('style', 'color: green')
-    labelDataNascimento.innerHTML = 'Data de Nascimento'
-    dataNascimento.setAttribute('style', 'border-color: green')
-    validDataNascimento = true
+    labelDataNascimento.setAttribute('style', 'color: green');
+    labelDataNascimento.innerHTML = 'Data de Nascimento';
+    dataNascimento.setAttribute('style', 'border-color: green');
+    validDataNascimento = true;
   }
-})
+});
 
 usuario.addEventListener('keyup', () => {
-  if(usuario.value.length <= 4){
-    labelUsuario.setAttribute('style', 'color: red')
-    labelUsuario.innerHTML = 'Usuário *Insira no minimo 5 caracteres'
-    usuario.setAttribute('style', 'border-color: red')
-    validUsuario = false
+  if (usuario.value.length <= 4) {
+    labelUsuario.setAttribute('style', 'color: red');
+    labelUsuario.innerHTML = 'Usuário *Insira no mínimo 5 caracteres';
+    usuario.setAttribute('style', 'border-color: red');
+    validUsuario = false;
   } else {
-    labelUsuario.setAttribute('style', 'color: green')
-    labelUsuario.innerHTML = 'Usuário'
-    usuario.setAttribute('style', 'border-color: green')
-    validUsuario = true
+    labelUsuario.setAttribute('style', 'color: green');
+    labelUsuario.innerHTML = 'Usuário';
+    usuario.setAttribute('style', 'border-color: green');
+    validUsuario = true;
   }
-})
+});
 
 senha.addEventListener('keyup', () => {
-  if(senha.value.length <= 5){
-    labelSenha.setAttribute('style', 'color: red')
-    labelSenha.innerHTML = 'Senha *Insira no minimo 6 caracteres'
-    senha.setAttribute('style', 'border-color: red')
-    validSenha = false
+  if (senha.value.length <= 5) {
+    labelSenha.setAttribute('style', 'color: red');
+    labelSenha.innerHTML = 'Senha *Insira no mínimo 6 caracteres';
+    senha.setAttribute('style', 'border-color: red');
+    validSenha = false;
   } else {
-    labelSenha.setAttribute('style', 'color: green')
-    labelSenha.innerHTML = 'Senha'
-    senha.setAttribute('style', 'border-color: green')
-    validSenha = true
+    labelSenha.setAttribute('style', 'color: green');
+    labelSenha.innerHTML = 'Senha';
+    senha.setAttribute('style', 'border-color: green');
+    validSenha = true;
   }
-})
+});
 
 confirmSenha.addEventListener('keyup', () => {
-  if(senha.value != confirmSenha.value){
-    labelConfirmSenha.setAttribute('style', 'color: red')
-    labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
-    confirmSenha.setAttribute('style', 'border-color: red')
-    validConfirmSenha = false
+  if (senha.value !== confirmSenha.value) {
+    labelConfirmSenha.setAttribute('style', 'color: red');
+    labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem';
+    confirmSenha.setAttribute('style', 'border-color: red');
+    validConfirmSenha = false;
   } else {
-    labelConfirmSenha.setAttribute('style', 'color: green')
-    labelConfirmSenha.innerHTML = 'Confirmar Senha'
-    confirmSenha.setAttribute('style', 'border-color: green')
-    validConfirmSenha = true
+    labelConfirmSenha.setAttribute('style', 'color: green');
+    labelConfirmSenha.innerHTML = 'Confirmar Senha';
+    confirmSenha.setAttribute('style', 'border-color: green');
+    validConfirmSenha = true;
   }
-})
+});
 
-function cadastrar(){
-  if(validNome && validUsuario && validSenha && validConfirmSenha && validDataNascimento){
-
+function cadastrar() {
+  if (validNome && validUsuario && validSenha && validConfirmSenha && validDataNascimento) {
     var generoSelecionado = obterGeneroSelecionado();
     var nivelSelecionado = obterNivelSelecionadao();
-    
-    let listaUser = 
-    {
+
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    let ultimoId = parseInt(localStorage.getItem("ultimoIdUsuario") || "0");
+
+    let listaUser = {
+      id: ultimoId + 1,
       nomeCad: nome.value,
       userCad: usuario.value,
       senhaCad: senha.value,
       generoCad: generoSelecionado,
       nivelCad: nivelSelecionado,
       dataNascimentoCad: dataNascimento.value
-
     };
 
-    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
     usuarios.push(listaUser);
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
-  
-    
-   
-    msgSuccesso.setAttribute('style', 'display: block')
-    msgSuccesso.innerHTML = '<strong>Cadastrando usuário...</strong>'
-    msgErro.setAttribute('style', 'display: none')
-    msgErro.innerHTML = ''
+    localStorage.setItem("ultimoIdUsuario", listaUser.id);
+
+    msgSuccesso.setAttribute('style', 'display: block');
+    msgSuccesso.innerHTML = '<strong>Cadastrando usuário...</strong>';
+    msgErro.setAttribute('style', 'display: none');
+    msgErro.innerHTML = '';
     setTimeout(() => {
       window.location.href = '../pages/signin.html';
-  }, 3000);
-    
+    }, 3000);
   } else {
-    msgErro.setAttribute('style', 'display: block')
-    msgErro.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>'
-    msgSuccesso.innerHTML = ''
-    msgSuccesso.setAttribute('style', 'display: none')
+    msgErro.setAttribute('style', 'display: block');
+    msgErro.innerHTML = '<strong>Preencha todos os campos corretamente antes de cadastrar</strong>';
+    msgSuccesso.innerHTML = '';
+    msgSuccesso.setAttribute('style', 'display: none');
   }
 }
-
 
 function obterGeneroSelecionado() {
   var inputs = document.querySelectorAll('.gender-group input[type="radio"]');
   var generoSelecionado = "";
   inputs.forEach(function(input) {
-      if (input.checked) {
-          generoSelecionado = input.value;
-      }
+    if (input.checked) {
+      generoSelecionado = input.value;
+    }
   });
   return generoSelecionado;
 }
@@ -150,34 +146,29 @@ function obterNivelSelecionadao() {
   var inputs = document.querySelectorAll('.nivel-group input[type="radio"]');
   var nivelSelecionado = "";
   inputs.forEach(function(input) {
-      if (input.checked) {
-          nivelSelecionado = input.value;
-      }
+    if (input.checked) {
+      nivelSelecionado = input.value;
+    }
   });
   return nivelSelecionado;
 }
 
-btn.addEventListener('click', ()=>{
-  let inputSenha = document.querySelector('#senha')
-  
-  if(inputSenha.getAttribute('type') == 'password'){
-    inputSenha.setAttribute('type', 'text')
+btn.addEventListener('click', () => {
+  let inputSenha = document.querySelector('#senha');
+
+  if (inputSenha.getAttribute('type') == 'password') {
+    inputSenha.setAttribute('type', 'text');
   } else {
-    inputSenha.setAttribute('type', 'password')
+    inputSenha.setAttribute('type', 'password');
   }
-})
+});
 
-btnConfirm.addEventListener('click', ()=>{
-  let inputConfirmSenha = document.querySelector('#confirmSenha')
-  
-  if(inputConfirmSenha.getAttribute('type') == 'password'){
-    inputConfirmSenha.setAttribute('type', 'text')
+btnConfirm.addEventListener('click', () => {
+  let inputConfirmSenha = document.querySelector('#confirmSenha');
+
+  if (inputConfirmSenha.getAttribute('type') == 'password') {
+    inputConfirmSenha.setAttribute('type', 'text');
   } else {
-    inputConfirmSenha.setAttribute('type', 'password')
+    inputConfirmSenha.setAttribute('type', 'password');
   }
-})
-
-
-
-  
-  
+});
